5b86a5397d67db68b6e0d952e0f9b80bc8bd1f6e
Ama_LS367 <levonlevonyanxx@gmail.com>
2025-05-31 17:54:00 +0400

    final version

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,13 @@
+#config/urls.py
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Главная страница работает!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # Главная страница
 ]
+
